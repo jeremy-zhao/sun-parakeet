0f72a5ad4c5f43b8bca6726bbcafe7d244e81a4f
zhaozhu <zhaozhu@yestargroup.com>
2025-03-17 16:51:01 +0800

    demo

--- src/routes/demo/layout/auto-center/+page.svelte
+++ src/routes/demo/layout/auto-center/+page.svelte
@@ -1,18 +1,20 @@
 <script lang="ts" module>
-  import { Page, AutoCenter } from '$lib'
+  import { Page, List, AutoCenter } from '$lib'
 </script>
 
 <Page class="bg-gray-100">
-  <h1 class="px-4 py-2">内容不够整行时自动居中</h1>
-  <AutoCenter class="bg-white px-4 py-2">et laborum ex</AutoCenter>
-  <h1 class="px-4 py-2">内容达到满宽后保持正常的左对齐</h1>
-  <AutoCenter class="bg-white px-4 py-2">
-    Cupidatat adipisicing id esse cupidatat aute excepteur nostrud. Qui nisi ea duis ex eiusmod qui sunt tempor dolore
-    incididunt cupidatat. Consequat qui fugiat sint. Nisi elit nulla sit aliquip sit elit est proident cillum in
-    adipisicing nulla consequat ex dolor. Ipsum mollit et fugiat tempor eiusmod anim magna veniam est dolor qui magna ut
-    veniam cupidatat. Ut ad eu proident culpa consequat dolore deserunt enim elit. Eu enim veniam irure ipsum sint.
-    Officia eu proident exercitation excepteur mollit magna velit cillum esse. Dolor proident laboris reprehenderit
-    commodo dolore exercitation officia mollit in commodo irure. Ad minim non esse anim do excepteur. Eiusmod Lorem
-    nulla enim Lorem exercitation ad officia.
-  </AutoCenter>
+  <List header="内容不够整行时自动居中">
+    <AutoCenter class="bg-white px-4 py-2">et laborum ex</AutoCenter>
+  </List>
+  <List header="内容达到满宽后保持正常的左对齐">
+    <AutoCenter class="bg-white px-4 py-2">
+      Cupidatat adipisicing id esse cupidatat aute excepteur nostrud. Qui nisi ea duis ex eiusmod qui sunt tempor dolore
+      incididunt cupidatat. Consequat qui fugiat sint. Nisi elit nulla sit aliquip sit elit est proident cillum in
+      adipisicing nulla consequat ex dolor. Ipsum mollit et fugiat tempor eiusmod anim magna veniam est dolor qui magna
+      ut veniam cupidatat. Ut ad eu proident culpa consequat dolore deserunt enim elit. Eu enim veniam irure ipsum sint.
+      Officia eu proident exercitation excepteur mollit magna velit cillum esse. Dolor proident laboris reprehenderit
+      commodo dolore exercitation officia mollit in commodo irure. Ad minim non esse anim do excepteur. Eiusmod Lorem
+      nulla enim Lorem exercitation ad officia.
+    </AutoCenter>
+  </List>
 </Page>

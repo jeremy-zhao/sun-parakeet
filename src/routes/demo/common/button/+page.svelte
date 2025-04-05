d43d1ed8b41162ef00e91dc975ad0501e7db199c
= <=>
2025-04-05 10:59:41 +0800

    行高问题调整

--- src/routes/demo/common/button/+page.svelte
+++ src/routes/demo/common/button/+page.svelte
@@ -1,17 +1,19 @@
 <script lang="ts" module>
-  import { Page, Button } from '$lib'
+  import { Page, Button, Icon, Space } from '$lib'
 </script>
 
 <Page class="px-4">
-  <Button class="my-2" color="normal">OK</Button>
-  <Button class="my-2" color="primary" shape="rounded">确定</Button>
-  <Button class="my-2" color="primary-plain">取消</Button>
-  <Button class="my-2" color="success" shape="rectangular">成功</Button>
-  <Button class="my-2" color="success-plain">成功</Button>
-  <Button class="my-2" color="warning">警告</Button>
-  <Button class="my-2" color="warning-plain">警告</Button>
-  <Button class="my-2" color="danger">危险</Button>
-  <Button class="my-2" color="danger-plain">危险</Button>
-  <Button class="my-2" color="text">文本</Button>
-  <Button class="my-2" disabled>禁用</Button>
+  <Space wrap>
+    <Button class="my-2" color="normal">👌OK</Button>
+    <Button class="my-2" color="primary" shape="rounded"><Icon name="check" />确定</Button>
+    <Button class="my-2" color="primary-plain">取消</Button>
+    <Button class="my-2" color="success" shape="rectangular">成功</Button>
+    <Button class="my-2" color="success-plain">成功</Button>
+    <Button class="my-2" color="warning">警告</Button>
+    <Button class="my-2" color="warning-plain">警告</Button>
+    <Button class="my-2" color="danger">危险</Button>
+    <Button class="my-2" color="danger-plain">危险</Button>
+    <Button class="my-2" color="text">文本</Button>
+    <Button class="my-2" color="primary-plain" disabled>禁用</Button>
+  </Space>
 </Page>

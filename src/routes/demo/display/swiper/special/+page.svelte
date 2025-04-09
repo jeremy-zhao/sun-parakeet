e46d52cd155d5f77c470179087e165fa4b5ab221
zhaozhu <zhaozhu@yestargroup.com>
2025-04-09 13:10:47 +0800

    宽度控制和无限滚动先不做了。

--- src/routes/demo/display/swiper/special/+page.svelte
+++ src/routes/demo/display/swiper/special/+page.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
-  import { Page, List, Swiper, Button, Popup } from '$lib'
+  import { Page, List, Swiper, Button, Popup, Space } from '$lib'
 
   // const colors = ['#ace0ff', '#bcffbd', '#e4fabd', '#ffcfac', 'pink']
-  // const colors = ['#ace0ff', '#bcffbd', '#e4fabd', '#ffcfac']
+  const colors = ['#ace0ff', '#bcffbd', '#e4fabd', '#ffcfac']
   // const colors = ['#ace0ff', '#bcffbd', '#e4fabd']
-  const colors = ['#ace0ff', '#bcffbd']
+  // const colors = ['#ace0ff', '#bcffbd']
   // const colors = ['#ace0ff']
 
   let visible = $state(false)
 </script>
 
-{#snippet items()}
+{#snippet items(colors: string[])}
   {#each colors as color, index}
     <div
       class="flex items-center justify-center text-white"
@@ -22,17 +22,26 @@
 {/snippet}
 
 <Page class="bg-gray-100">
-  <List header="指示器颜色">
+  <List header="全屏引导">
     <div class="bg-white p-4">
       <Button onclick={() => (visible = true)}>显示弹出层</Button>
     </div>
   </List>
   <Popup bind:visible class="h-full" destroyOnClose>
     <Swiper class="h-full">
-      {@render items()}
+      {@render items(colors.slice(0, 2))}
       <div class="flex items-center justify-center bg-white text-white">
         <Button onclick={() => (visible = false)}>开始使用</Button>
       </div>
     </Swiper>
   </Popup>
+  <!-- <List header="宽度控制">
+    <Space class="bg-white p-4" direction="vertical" block>
+      <Swiper style="height: 120px" --sp-width="75%">
+        {@render items(colors)}
+      </Swiper>
+      <p class="text-sm text-gray-600">通过 --sp-width --sp-height 控制内部元素的宽高</p>
+    </Space>
+  </List> -->
+  <p></p>
 </Page>
